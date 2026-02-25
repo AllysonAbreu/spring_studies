97d359bdc91618d2b5c3d97ef673040031eeefb4
allyson <allyson.abreu@cwi.com.br>
2026-02-25 10:43:31 -0300

    feat: Revisando conceitos básicos de spring e spring boot

---
+++ src/test/java/br/com/unipds/spring/SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unipds.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
